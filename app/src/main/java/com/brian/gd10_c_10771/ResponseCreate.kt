7419ba213237b3866e9cf30ead28ce8ed91f06d1
doni255 <wijayadoni37@gmail.com>
2022-11-10 20:26:12 +0700

    Initial commit

---
+++ app/src/main/java/com/brian/gd10_c_10771/ResponseCreate.kt
@@ -0,0 +1,9 @@
+package com.brian.gd10_c_10771
+
+import com.google.gson.annotations.SerializedName
+
+class ResponseCreate (
+    @SerializedName("status") val stt:Int,
+    @SerializedName("error") val e:Boolean,
+    @SerializedName("message") val pesan:String,
+)
